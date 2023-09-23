79d7089f71377c883bb4f45ec0fc95dbd3ce3109
Vesa Rautiainen <veskurau@gmail.com>
2023-09-23 13:49:03 +0300

    Update database tables and imports

--- routes.py
+++ routes.py
@@ -1,8 +1,5 @@
-from os import getenv
 from flask import redirect, render_template, request, session
 from app import app
-
-app.secret_key = getenv("SECRET_KEY")
 
 
 @app.route("/")
--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    name TEXT,
+    username TEXT UNIQUE,
     password TEXT,
     role INTEGER
 );
