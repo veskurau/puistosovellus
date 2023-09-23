94058dec87ee80ff15f0d2f0adae41f2bce5ba54
Vesa Rautiainen <veskurau@gmail.com>
2023-09-23 14:55:47 +0300

    Add login page

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from os import getenv
+from dotenv import load_dotenv
 from flask import Flask
 
+load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
--- routes.py
+++ routes.py
@@ -1,7 +1,22 @@
+#from os import getenv
 from flask import redirect, render_template, request, session
 from app import app
 
+#app.secret_key = getenv("SECRET_KEY")
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    # TODO: usernamen ja passwordin tarkastus
+    session["username"] = username
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
