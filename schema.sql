c977638df24f383cd619f993f85d7c9ce6d83a9e
Vesa Rautiainen <veskurau@gmail.com>
2023-09-24 11:41:16 +0300

    Add tables parks and reviews to schema.sql

--- schema.sql
+++ schema.sql
@@ -4,3 +4,17 @@ CREATE TABLE users (
     password TEXT,
     admin BOOLEAN
 );
+
+CREATE TABLE parks (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    description TEXT,
+    address TEXT
+);
+
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    comment TEXT,
+    points INTERGER,
+    parks_id INTERGER REFERENCES parks
+);
